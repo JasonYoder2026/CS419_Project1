92aa08cc75212b7158b61648b83c352eb8f717fc
Jason Yoder <jason.yoder2@bsu.edu>
2025-09-10 14:47:47 -0400

    Adds sjf algorithm

--- src/RR.java
+++ src/RR.java
@@ -1,14 +1,64 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * TODO: implement the RR (Round Robin) scheduling algorithm
  */
 public class RR extends Algorithm{
 
+    private final Queue<Process> readyQueue = new LinkedList<>();
+
+    // Processes that have not yet arrived
+    private final Queue<Process> processesToArrive;
+
+    // The simulation clock
+    private int now = 0;
+
+
     public RR(List<Process> allProcesses){
         super(allProcesses);
+        processesToArrive = new LinkedList<>(allProcesses);
     }
 
     @Override
-    public void schedule(){}
+    public void schedule(){
+        System.out.println("Round Robin");
+
+        while (!readyQueue.isEmpty() || !processesToArrive.isEmpty()) {
+            if (readyQueue.isEmpty()) {
+                Process process = processesToArrive.remove();
+                if (now < process.getArrivalTime()) {
+                    //advance the simulation clock to the next process's arrival time
+                    now = process.getArrivalTime();
+                }
+                readyQueue.add(process);
+            }
+
+            // With FCFS scheduling, the next process to schedule is the one
+            // at the head of the ready queue
+            Process currentProcess = readyQueue.remove();
+
+            // With FCFS there is no preemption, so a thread can always run
+            // to completion.
+            int runTime = currentProcess.getBurstTime();
+
+            // Execute the selected process
+            System.out.print("At time " + now + ": ");
+            CPU.run(currentProcess, runTime);
+
+            // Advance the simulation clock
+            now += runTime;
+
+            // Mark this process as completed
+            currentProcess.setRemainingTime(0);
+            currentProcess.setFinishTime(now);
+
+            // If any processes have arrived by 'now', add them to ready queue
+            while(!processesToArrive.isEmpty() &&
+                    processesToArrive.peek().getArrivalTime()<=now){
+                readyQueue.add(processesToArrive.remove());
+            }
+        }
+    }
 }
--- src/SJF.java
+++ src/SJF.java
@@ -6,12 +6,57 @@ import java.util.*;
  */
 
 public class SJF extends Algorithm {
+    private final LinkedList<Process> readyQueue = new LinkedList<>();
+
+    // Processes that have not yet arrived
+    private final Queue<Process> processesToArrive;
+
+    // The simulation clock
+    private int now = 0;
 
     public SJF(List<Process> allProcessList){
         super(allProcessList);
+        processesToArrive = new LinkedList<>(allProcessList);
     }
 
     @Override
     public void schedule() {
+        System.out.println("Shortest Job First:");
+
+        while (!readyQueue.isEmpty() || !processesToArrive.isEmpty()) {
+            if (readyQueue.isEmpty()) {
+                Process process = processesToArrive.remove();
+                if (now < process.getArrivalTime()) {
+                    //advance the simulation clock to the next process's arrival time
+                    now = process.getArrivalTime();
+                }
+                readyQueue.add(process);
+            }
+
+
+            Process currentProcess = readyQueue.getFirst();
+
+            for (int i = 0; i < readyQueue.size(); i++) {
+                if (currentProcess.getBurstTime() <= readyQueue.get(i).getBurstTime()) {
+                    currentProcess = readyQueue.get(i);
+                }
+            }
+            readyQueue.remove(currentProcess);
+
+
+            int runTime = currentProcess.getBurstTime();
+            System.out.print("At time " + now + ": ");
+            CPU.run(currentProcess, runTime);
+
+            now += runTime;
+
+            currentProcess.setRemainingTime(0);
+            currentProcess.setFinishTime(now);
+
+            // If any processes have arrived by 'now', add them to ready queue
+            while(!processesToArrive.isEmpty() && processesToArrive.peek().getArrivalTime()<=now){
+                readyQueue.add(processesToArrive.remove());
+            }
+        }
     }
 }
